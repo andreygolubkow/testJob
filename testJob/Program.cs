6468728aa2bf673da701861be5c6c69a9c0e1241
andreygolubkow <andreygolubkow@gmail.com>
2017-03-31 10:34:10 +0700

    Считываение данных из файла

--- testJob/Program.cs
+++ testJob/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace testJob
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using testJob.Model;
+
+namespace testJob
 {
     static class Program
     {
         private static void Main(string[] args)
         {
-            
+            var fileInfo = new FileInfo(args[1]);
+            if ( !fileInfo.Exists )
+            {
+                Console.WriteLine("Ошибка в файле.");
+                return;
+            }
+            var fileReader = new StreamReader(args[1]);
+            var ordersList = new List<Order>();
+            FileReader.Rows[] rowses = FileReader.ReadRowses(fileReader);
+            while ( !fileReader.EndOfStream )
+            {
+                ordersList.Add(FileReader.ReadOrder(fileReader,rowses));
+            }
 
         }
     }
