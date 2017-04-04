5c63928e11701527297d0353dc193dfac80c3350
andreygolubkow <andreygolubkow@gmail.com>
2017-04-05 03:07:01 +0700

    Настройки

--- testJob/Properties/AssemblyInfo.cs
+++ testJob/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("testJob")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Задание разработчик-стажер C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("testJob")]
